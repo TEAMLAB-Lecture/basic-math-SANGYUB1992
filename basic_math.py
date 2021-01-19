a2895a1e93051d6c0fae3e5f70f648de017e5ef3
SANGYUB1992 <studyfia1992@gmail.com>
2021-01-19 14:32:45 +0900

    Update my code

--- basic_math.py
+++ basic_math.py
@@ -24,8 +24,9 @@ def get_greatest(number_list):
             >>> bm.get_greatest(number_list)
             99
     """
-    greatest_number = None
-    return greatest_number
+    
+    
+    return max(number_list)
 
 
 def get_smallest(number_list):
@@ -45,10 +46,10 @@ def get_smallest(number_list):
             >>> bm.get_smallest(number_list)
             11
     """
-    smallest_number = None
-    return smallest_number
 
+    return min(number_list)
 
+print(
 def get_mean(number_list):
     """
     주어진 리스트 숫자들의 평균을 구함.
@@ -66,8 +67,8 @@ def get_mean(number_list):
             >>> bm.get_mean(number_list)
             47
     """
-    mean = None
-    return mean
+    
+    return sum(number_list)/len(number_list)
 
 
 def get_median(number_list):
@@ -90,5 +91,11 @@ def get_median(number_list):
             >>> bm.get_median(number_list2)
             35.5
     """
-    median = None
-    return median
+    number_list.sort()
+
+    if len(number_list)%2==1:
+        return number_list[len(number_list)//2]
+    else:
+        a=len(number_list)//2
+        b=a-1
+        return (number_list[a]+number_list[b])/2
